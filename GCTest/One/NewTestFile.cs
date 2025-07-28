9408e9cef205e0dc2de649d18ae3af4226b36744
Sebastian Hernandez <sebhernandez@microsoft.com>
2025-07-28 10:14:54 -0700

    new commit

---
+++ GCTest/One/NewTestFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCTest.One
+{
+    internal class NewTestFile
+    {
+        public void Foo()
+        {
+            Console.WriteLine("New Commit");
+        }
+    }
+}
