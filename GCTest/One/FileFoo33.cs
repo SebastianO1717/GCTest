b0793d40c76610af3f5762bfe4bc6b05dc1d3a84
Sebastian Hernandez <sebhernandez@microsoft.com>
2025-08-20 21:03:06 -0700

    33

---
+++ GCTest/One/FileFoo33.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCTest.One
+{
+    internal class FileFoo33
+    {
+    }
+}
